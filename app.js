1f98f1e0fb329fc384777c45ba519e4d2383fe1e
shyamraja <t7bhsh00@students.oamk.fi>
2019-01-26 18:26:53 +0200

    Modifed app.js and routes for getting grades of selected students

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const students = require("./routes/api/students");
 const courses  = require("./routes/api/courses");
-
+const grades = require("./routes/api/grades");
 const app      = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +13,7 @@ app.get('/test', (req, res) => res.json({msg:"It is working"}))
 
 app.use('/api/students', students);
 app.use('/api/courses', courses);
-
+app.use('/api/students/:student_id/grades', grades);
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
