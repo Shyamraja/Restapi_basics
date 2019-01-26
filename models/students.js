4ccf217016a8d47c2c0fbba9d7be04f8b23579bb
shyamraja <t7bhsh00@students.oamk.fi>
2019-01-26 18:17:17 +0200

    Students.js added in models for viewing grade results

---
+++ models/students.js
@@ -0,0 +1,45 @@
+const students = 
+    [
+        {
+            Id: 1,
+            name:"Shyam Vetwal",
+            Address:"Kajannintie 30 90120 Oulu",
+            Courses:[
+                {   
+                    id: 1,
+                    grade: 3,
+                    name: "Java",
+                },
+                {
+                    id: 2,
+                    grade:1,
+                    name: "Android Application",
+                },
+                {
+                    id: 3,
+                    grade:4,
+                    name:"AngularJS",
+                }
+            ],
+        },
+        {
+            Id: 2,
+            name:"Suman Pandey",
+            Address:"Hanhitie 17 90150 Oulu",
+            Courses:[
+                {
+                    id: 1,
+                    grade: 4,
+                    name: "Php",
+                },
+                {
+                    id:3,
+                    grade:2,
+                    name:"AngularJS",
+                }
+            ],
+        }
+       
+    ];
+
+module.exports = students;
